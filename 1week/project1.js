391b9d7317a35e2b9bd19836b0aaa72e9f3ba796
tjrwnwkd21 <tjrwnwkd21@naver.com>
2019-05-16 18:18:06 +0900

    zzz

--- 1week/project1.js
+++ 1week/project1.js
@@ -1,57 +1,54 @@
-var _promise = function (param) {
-
-	return new Promise(function (resolve, reject) {
-
+var promise = function (text) {
 		// 비동기를 표현하기 위해 setTimeout 함수를 사용 
-		window.setTimeout(function () {
-
-			// 파라메터가 참이면, 
-			if (param) {
-
-				// 해결됨 
-				resolve("해결 완료");
-			}
-
-			// 파라메터가 거짓이면, 
-			else {
-
-				// 실패 
-				reject(Error("실패!"));
-			}
-		}, 3000);
-	});
+	window.setTimeout(function () {
+		if (text == 'hello'){
+			readTextFile("./txt_hello.txt");
+		}
+		else if (text == 'name'){
+			readTextFile("./txt_name.txt");
+		}
+		else readTextFile("./txt_bye.txt");
+	}, 3000);
 };
 
-//Promise 실행
-
-
-
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest(); 
+    rawFile.open("GET", file, false); 
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    };
+    rawFile.send(null);
+}
 
 
 function fnc(work){
 	switch(work){
-		case 'callback' :
-			_promise(true)
-			.then(function (text) {
-				// 성공시
-				console.log(text);
-			}, function (error) {
-				// 실패시 
-				console.error(error);
-			});
-			break;
-		
 
-			
+		case 'callback' :
+			readTextFile("./txt_hello.txt");
+			fnc('callback2');	
+			break;
+
+		case 'callback2' :
+			readTextFile("./txt_name.txt");			
+			fnc('callback3');
+			break;
+
+		case 'callback3' :
+			readTextFile("./txt_bye.txt");			
+			break;
+
+
 		case 'promise' :
-			_promise(true)
-			.then(function (text) {
-				// 성공시
-				console.log(text);
-			}, function (error) {
-				// 실패시 
-				console.error(error);
-			});
+			promise()
+				.then(function(hello))
+				.then(function(name))
+				.then(function(bye));
 			break;
 		
 		
